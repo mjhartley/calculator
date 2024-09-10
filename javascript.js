77ed5825b050e5e534a4bff63190e10f9045fba3
mjhartley <mike@the-hartleys.co.uk>
2024-09-10 11:10:58 +0100

    initial working version

--- javascript.js
+++ javascript.js
@@ -1,8 +1,10 @@
 // constants
 
 const display = document.querySelector(".display");
-let workingStorage = null;
-
+let workingStorage = 0;
+let num1 = "";
+let num2 = "";
+let wsAction;
 
 
 //Add event listeners to buttons
@@ -12,12 +14,59 @@ const myNumButtons = document.querySelectorAll(".button");
 myNumButtons.forEach((button) => {
     button.addEventListener('click', function(e) {
         console.log(e.target.innerText);
-        if (workingStorage === null){
-            workingStorage = e.target.innerText;
-        } else {
-            workingStorage = add(workingStorage, e.target.innerText)
+        console.log(e.target.dataset.action);
+        console.log("wsAction: " + wsAction);
+
+        // perhaps do with with a case statement
+
+        if(e.target.dataset.action == "add"){
+            console.log("Setting WSAction to add");
+            wsAction = "add";
+            return;
         }
-        display.innerText = workingStorage;
+
+        if(e.target.dataset.action == "subtract"){
+            wsAction = "subtract";
+            return;
+        }
+
+        if(e.target.dataset.action == "divide"){
+            wsAction = "divide";
+            return;
+        }
+
+        if(e.target.dataset.action == "multiply"){
+            wsAction = "multiply";
+            return;
+        }
+
+        if(e.target.dataset.action == "clear"){
+            clear();
+            return;
+        }
+
+
+        if(e.target.dataset.action == "equals"){
+            calculate(num1, num2, wsAction);
+            return;
+        }
+
+        // store the input for num1 while action is undefined 
+
+        if (wsAction == undefined ){
+            num1 = num1 + e.target.innerText;
+            console.log("Num 1 is " + num1 );
+
+        }
+
+        if (wsAction !== undefined){
+            num2 = num2 + e.target.innerText;
+            console.log("Num 2 is " + num2 );
+        }
+
+
+
+
     });
 });
 
@@ -30,13 +79,38 @@ function add(num1, num2){
 }
 
 function subtract(num1, num2){
+    console.log("Subtracting :" + num1 + "and " + num2);
     return num1 - num2;
 }
 
 function divide(num1, num2){
+    console.log("Dividing :" + num1 + "and " + num2);
     return num1 / num2;
 }
 
 function multiply(num1, num2){
+    console.log("Multiplying :" + num1 + "and " + num2);
     return num1 * num2
 }
+
+function calculate (num1, num2, operation){
+    if(operation == "add"){
+        display.innerText = add(num1, num2);
+    }
+
+    if(operation == "divide"){
+        display.innerText = divide(num1, num2);
+    }
+
+    if(operation == "divide"){
+        display.innerText = divide(num1, num2);
+    }
+}
+
+function clear (){
+    num1 = "";
+    num2 = "";
+    wsAction = "";
+    display.innerText = "0";
+}
+
