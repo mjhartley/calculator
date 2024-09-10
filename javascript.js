3d4b16de467f191b7a3da2c30079c4c99d1680ca
mjhartley <mike@the-hartleys.co.uk>
2024-09-10 13:29:26 +0100

    updated version - basic working functions

--- javascript.js
+++ javascript.js
@@ -4,7 +4,8 @@ const display = document.querySelector(".display");
 let workingStorage = 0;
 let num1 = "";
 let num2 = "";
-let wsAction;
+let wsAction = "";
+let displayText = ""; 
 
 
 //Add event listeners to buttons
@@ -16,27 +17,35 @@ myNumButtons.forEach((button) => {
         console.log(e.target.innerText);
         console.log(e.target.dataset.action);
         console.log("wsAction: " + wsAction);
+        updateDisplay(e.target.innerText);
 
         // perhaps do with with a case statement
 
-        if(e.target.dataset.action == "add"){
+        if(e.target.dataset.action == "+"){
             console.log("Setting WSAction to add");
-            wsAction = "add";
+            wsAction = "+";
+            updateDisplay(wsAction)
             return;
         }
 
-        if(e.target.dataset.action == "subtract"){
-            wsAction = "subtract";
+        if(e.target.dataset.action == "-"){
+            console.log("Setting WSAction to subtract");
+            wsAction = "-";
+            updateDisplay(wsAction)
             return;
         }
 
-        if(e.target.dataset.action == "divide"){
-            wsAction = "divide";
+        if(e.target.dataset.action == "/"){
+            console.log("Setting WSAction to divide");
+            wsAction = "/";
+            updateDisplay(wsAction)
             return;
         }
 
-        if(e.target.dataset.action == "multiply"){
-            wsAction = "multiply";
+        if(e.target.dataset.action == "*"){
+            console.log("Setting WSAction to multiply");
+            wsAction = "*";
+            updateDisplay(wsAction)
             return;
         }
 
@@ -53,15 +62,17 @@ myNumButtons.forEach((button) => {
 
         // store the input for num1 while action is undefined 
 
-        if (wsAction == undefined ){
+        if (wsAction == "" ){
             num1 = num1 + e.target.innerText;
             console.log("Num 1 is " + num1 );
+            updateDisplay(num1);
 
         }
 
-        if (wsAction !== undefined){
+        if (wsAction !== ""){
             num2 = num2 + e.target.innerText;
-            console.log("Num 2 is " + num2 );
+            console.log("Num2 is " + num2 );
+            updateDisplay(num2);
         }
 
 
@@ -74,37 +85,48 @@ myNumButtons.forEach((button) => {
 
 
 function add(num1, num2){
-    console.log("Adding :" + num1 + "and " + num2);
+    console.log("Adding : " + num1 + "and " + num2);
     return parseInt(num1) + parseInt(num2);
 }
 
 function subtract(num1, num2){
-    console.log("Subtracting :" + num1 + "and " + num2);
-    return num1 - num2;
+    console.log("Subtracting : " + num1 + "and " + num2);
+    return parseInt(num1) - parseInt(num2);
 }
 
 function divide(num1, num2){
-    console.log("Dividing :" + num1 + "and " + num2);
-    return num1 / num2;
+    console.log("Dividing : " + num1 + "and " + num2);
+    return parseInt(num1) / parseInt(num2);
 }
 
 function multiply(num1, num2){
-    console.log("Multiplying :" + num1 + "and " + num2);
-    return num1 * num2
+    console.log("Multiplying : " + num1 + "and " + num2);
+    return parseInt(num1) * parseInt(num2);
 }
 
-function calculate (num1, num2, operation){
-    if(operation == "add"){
-        display.innerText = add(num1, num2);
+function calculate (calNum1, calNum2, operation){
+    if(operation == "+"){
+        workingStorage = add(calNum1, calNum2);
     }
 
-    if(operation == "divide"){
-        display.innerText = divide(num1, num2);
+    if(operation == "/"){
+        workingStorage = divide(calNum1, calNum2);
     }
 
-    if(operation == "divide"){
-        display.innerText = divide(num1, num2);
+    if(operation == "-"){
+        workingStorage = subtract(calNum1, calNum2);
     }
+
+    if(operation == "*"){
+        workingStorage = multiply(calNum1, calNum2);
+
+    }
+
+    updateDisplay(workingStorage);
+    wsAction = "";
+    num1 = workingStorage;    
+    num2 = "";
+
 }
 
 function clear (){
@@ -112,5 +134,11 @@ function clear (){
     num2 = "";
     wsAction = "";
     display.innerText = "0";
+    workingStorage = "";
+    displayText = "";
+
 }
 
+function updateDisplay(displayText){
+    display.innerText = displayText;
+}
